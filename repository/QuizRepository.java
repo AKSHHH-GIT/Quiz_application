9df8ec8c28275ecd29395ab3db2a1d9259a9ca2c
AKSHHH-GIT <157200757+AKSHHH-GIT@users.noreply.github.com>
2025-10-06 11:22:32 +0530

    Delete repository directory

--- repository/QuizRepository.java
+++
@@ -1,9 +0,0 @@
-package com.verto.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.verto.demo.model.Quiz;
-
-// Repository interface for Quiz entity
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
-}
