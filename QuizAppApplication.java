0e64919bc7e95d5a49fcbd29e98c8156d2f5ab9e
AKSHHH-GIT <157200757+AKSHHH-GIT@users.noreply.github.com>
2025-10-06 11:15:05 +0530

    Add files via upload

---
+++ QuizAppApplication.java
@@ -0,0 +1,12 @@
+package com.verto.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Main entry point of the Spring Boot Application
+@SpringBootApplication
+public class QuizAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuizAppApplication.class, args);
+    }
+}
